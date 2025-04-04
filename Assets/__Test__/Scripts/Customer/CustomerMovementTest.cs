6182f6cd443fc59329c1ce95740cd8655f5d7f00
impactfrags <benjaminm4924049240@gmail.com>
2025-04-04 15:48:57 +0200

    HUD Updated Experience up to date
    Experience is now working and the HUD is displayed on start

--- Assets/__Test__/Scripts/Customer/CustomerMovementTest.cs
+++ Assets/__Test__/Scripts/Customer/CustomerMovementTest.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
 
-
 public class CustomerMovementTest : MonoBehaviour
 {
     private NavMeshAgent agent;
+    public int zoneRadius = 3;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    public IEnumerator MoveTo(Vector3 destination)
+    public IEnumerator MoveTo(Vector3 zoneCenter)
     {
+        Vector3 destination = GetRandomPointInZone(zoneCenter);
         agent.SetDestination(destination);
 
         while (agent.pathPending)
@@ -21,12 +22,18 @@ public class CustomerMovementTest : MonoBehaviour
             yield return null;
         }
 
-
         while (agent.remainingDistance > agent.stoppingDistance)
         {
             yield return null;
         }
+    }
 
+    private Vector3 GetRandomPointInZone(Vector3 zoneCenter)
+    {
+        int randomPosX = Random.Range(-zoneRadius, zoneRadius);
+        int randomPosZ = Random.Range(-zoneRadius, zoneRadius);
+        Vector3 destination = new Vector3(zoneCenter.x + randomPosX, 1, zoneCenter.z + randomPosZ);
 
+        return destination;
     }
 }
---
+++ Assets/__Test__/Scripts/Experience/ExperienceManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExperienceManager : MonoBehaviour
+{
+    private int currentLevel;
+    private int progression;
+
+    void Start()
+    {
+        SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+        currentLevel = 0;
+        progression = 0;
+    }
+    public void addProgression(int p)
+    {
+        if (progression + p > 100)
+        {
+            levelup();
+        }
+        else
+        {
+            progression += p;
+            updateExperienceBar();
+        }
+    }
+
+    public void levelup()
+    {
+        progression = 0;
+        currentLevel += 1;
+        updateExperienceBar();
+        updateLevel();
+    }
+
+    public void updateExperienceBar()
+    {
+        if (ExperienceBar.Instance != null)
+        {
+            ExperienceBar.Instance.DisplayXpBar(progression);
+        }
+        else
+        {
+            Debug.LogWarning("HUD pas encore prêt");
+        }
+    }
+    public void updateLevel()
+    {
+        if (ExperienceBar.Instance != null)
+        {
+            ExperienceBar.Instance.DisplayCurrentLevel(currentLevel);
+        }
+        else
+        {
+            Debug.LogWarning("HUD pas encore prêt");
+        }
+    }
+}
