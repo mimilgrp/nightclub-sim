b15e3c74d3d6679a61882a3737a51702647d6a0a
impactfrags <benjaminm4924049240@gmail.com>
2025-04-04 16:25:44 +0200

    Storage

--- Assets/__Test__/Scripts/Item/StorageItem.cs
+++ Assets/__Test__/Scripts/Item/StorageItem.cs
@@ -1,33 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StorageItem : MonoBehaviour
 {
     [Header("Storage Parameters")]
-    public int stockAmount = 0;
     public int stockCapacity = 120;
-    public string requiredTag = "";
+
+    private Dictionary<string, int> stockByType = new Dictionary<string, int>();
+
+    private int TotalStock => GetTotalStock();
 
     public void Interact(TakeDropItem carriedItem)
     {
-        if (!carriedItem.CompareTag(requiredTag))
+        string itemTag = carriedItem.tag;
+        int itemQuantity = carriedItem.itemQuantity;
+
+        if (string.IsNullOrEmpty(itemTag))
         {
-            Debug.Log("Shelf: Incorrect item tag");
+            Debug.LogWarning("L'objet transporté n'a pas de tag !");
+            return;
+        }
+
+        if (TotalStock + itemQuantity > stockCapacity)
+        {
+            Debug.Log($"Shelf: Capacité insuffisante ({TotalStock + itemQuantity} > {stockCapacity})");
+            return;
+        }
+
+        if (stockByType.ContainsKey(itemTag))
+        {
+            stockByType[itemTag] += itemQuantity;
         }
         else
         {
-            int itemQuantity = carriedItem.itemQuantity;
-
-            if ((stockAmount + itemQuantity) > stockCapacity)
-            {
-                Debug.Log("Shelf: Insufficient capacity (" + (stockAmount + itemQuantity) + " > " + stockCapacity + ")");
-            }
-            else
-            {
-                stockAmount += itemQuantity;
-                Destroy(carriedItem.gameObject);
-
-                Debug.Log("Shelf: Item added (new stock amount: " + stockAmount + ")");
-            }
+            stockByType[itemTag] = itemQuantity;
         }
+
+        Destroy(carriedItem.gameObject);
+        Debug.Log($"Shelf: {itemQuantity} {itemTag} ajoutés. Stock actuel : {stockByType[itemTag]} ({TotalStock}/{stockCapacity})");
+
+    }
+    private int GetTotalStock()
+    {
+        int total = 0;
+        foreach (int qty in stockByType.Values)
+        {
+            total += qty;
+        }
+        return total;
     }
 }
