f14834f87df64daf7f45015d730e2e6569d6355a
mimilgrp <mimil.group.49@gmail.com>
2025-04-14 23:38:48 +0200

    MoneyManager script
    add instance and decrease function

--- Assets/__Test__/Scripts/EventSystem/MoneyManager.cs
+++ Assets/__Test__/Scripts/EventSystem/MoneyManager.cs
@@ -6,7 +6,17 @@ public class MoneyManager: MonoBehaviour
 {
     public float money = 0;
 
-    public void IncreaseMoney(int amount)
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public static MoneyManager Instance { get; private set; }
+
+    public void IncreaseMoney(float amount)
     {
         money += amount;
 
@@ -16,7 +26,17 @@ public class MoneyManager: MonoBehaviour
         }
     }
 
-    public bool HasEnoughMoney(int amount)
+    public void DecreaseMoney(float amount)
+    {
+        money -= amount;
+
+        if (HUDDisplay.Instance != null)
+        {
+            HUDDisplay.Instance.SetMoney(money);
+        }
+    }
+
+    public bool HasEnoughMoney(float amount)
     {
         return (money >= amount);
     }
