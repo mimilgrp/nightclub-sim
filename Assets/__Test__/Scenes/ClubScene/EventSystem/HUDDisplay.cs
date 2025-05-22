b1c2833f188d8a0a7aff8e774a12672313cd704e
mimilgrp <mimil.group.49@gmail.com>
2025-05-22 21:22:23 +0200

    HUDDisplay script
    create show notification and welcome functions

--- Assets/__Test__/Scenes/ClubScene/EventSystem/HUDDisplay.cs
+++ Assets/__Test__/Scenes/ClubScene/EventSystem/HUDDisplay.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,7 +20,26 @@ public class HUDDisplay : MonoBehaviour
     [Header("Time")]
     public TextMeshProUGUI timeText;
 
-    void Update()
+    [Header("Notifications")]
+    public Transform notificationParent;
+    public GameObject notificationPrefab;
+    public float notificationDuration = 5f;
+
+    [Header("Welcome Notification")]
+    public GameObject welcomeNotificationPrefab;
+    public float welcomeNotificationDuration = 10f;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        ShowWelcomeNotification();
+    }
+    
+    private void Update()
     {
         float money = MoneyManager.Instance.money;
         float popularity = PopularityManager.Instance.popularity;
@@ -35,5 +56,45 @@ public class HUDDisplay : MonoBehaviour
         experienceBar.fillAmount = experience / experienceStep;
         levelText.text = $"{level}";
         timeText.text = $"{hours:00}:{minutes:00}";
+    }
+
+    public static HUDDisplay Instance { get; private set; }
+
+    public void ShowNotification(string message)
+    {
+        GameObject notification = Instantiate(notificationPrefab, notificationParent);
+        TMP_Text text = notification.GetComponentInChildren<TMP_Text>();
+        text.text = message.ToUpper();
+
+        CanvasGroup canvasGroup = notification.GetComponent<CanvasGroup>();
+        StartCoroutine(FadeNotification(notification, canvasGroup, notificationDuration));
+    }
+
+    private void ShowWelcomeNotification()
+    {
+        GameObject notification = Instantiate(welcomeNotificationPrefab, notificationParent);
+
+        CanvasGroup canvasGroup = notification.GetComponent<CanvasGroup>();
+        StartCoroutine(FadeNotification(notification, canvasGroup, welcomeNotificationDuration));
+    }
+
+    IEnumerator FadeNotification(GameObject notification, CanvasGroup canvasGroup, float duration)
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime * 2)
+        {
+            canvasGroup.alpha = t;
+            yield return null;
+        }
+        canvasGroup.alpha = 1;
+
+        yield return new WaitForSeconds(duration);
+
+        for (float t = 1; t > 0; t -= Time.deltaTime * 2)
+        {
+            canvasGroup.alpha = t;
+            yield return null;
+        }
+
+        Destroy(notification);
     }
 }
